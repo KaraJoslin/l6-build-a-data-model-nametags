d42a49ed2cd2a95d6f7c76220958306a3b32eec9
Kara_J <kjoslin@avc.edu>
2023-05-30 08:56:54 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const names = [
+  { person: "May May", id: "001" },
+  { person: "Morgan", id: "002" },
+  { person: "SteveO", id: "003" },
+  { person: "Jodi", id: "004" },
+  { person: "Ann Marie", id: "005" },
+  { person: "Alisha", id: "006" },
+  { person: "Linda", id: "007" }
+];
